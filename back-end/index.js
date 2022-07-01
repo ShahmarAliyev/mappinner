44f0312a03c01ca2e5fa806035175c16614a8d7b
ShahmarALiyev <saliyev2019@gmail.com>
2022-06-30 19:51:11 -0500

    Connected to MongoDb through Mongoose

--- back-end/index.js
+++ back-end/index.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const dotenv = require("dotenv");
 const app = express();
 
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Databse Connected");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
 app.listen(8800, () => {
+  4;
   console.log("Server Started");
 });
